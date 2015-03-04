99ec9c869a1e1d48393e6454b7bd555c2bdaeb1d
Dominika Bienkowska <bienkowska.dominika@gmail.com>
2015-03-04 21:20:53 +0100

    Initial Commit

---
+++ newThings/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  newThings
+//
+//  Created by Dominika on 04.03.2015.
+//  Copyright (c) 2015 DominikaBienkowska. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
